061e3342f7594b0d50110eef45d0bf834ba568bb
Vu4ll <salihab2005@gmail.com>
2025-05-06 19:57:59 +0300

    Footer'da sürüm bilgisi eklendi.

--- util/express.js
+++ util/express.js
@@ -6,6 +6,7 @@ const app = express();
 const path = require("path");
 const { env, APP_URL_W_PORT } = require("./config");
 const flashMiddleware = require("../middlewares/flashMiddleware");
+const pkg = require('../package.json');
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -16,5 +17,6 @@ app.use(flashMiddleware);
 app.use("/images", express.static(path.join(__dirname, "..", "views", "images")));
 app.use("/js", express.static(path.join(__dirname, "..", "views", "js")));
 app.use("/", require("../routes"));
+app.locals.version = pkg.version;
 
 app.listen(env.PORT, () => console.log(`Server running on ${APP_URL_W_PORT}`));
