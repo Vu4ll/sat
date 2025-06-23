f5fc50d7e67ad4dfd665b4bc407c6a0c3a41d913
Vu4ll <salihab2005@gmail.com>
2025-06-23 11:48:39 +0300

    Koyu tema da sayfa yenilendiği zaman kısa süreli görünen beyaz tema sorunu giderildi.

--- views/js/darkmode.js
+++ views/js/darkmode.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const currentTheme = localStorage.getItem("theme") || "light"
+    const currentTheme = localStorage.getItem("theme") || "light";
 
-    document.documentElement.setAttribute("data-bs-theme", currentTheme)
+    document.documentElement.setAttribute("data-bs-theme", currentTheme);
 
-    const themeToggle = document.getElementById("themeToggle")
+    const themeToggle = document.getElementById("themeToggle");
     if (themeToggle) {
-        themeToggle.checked = currentTheme === "dark"
+        themeToggle.checked = currentTheme === "dark";
 
         themeToggle.addEventListener("change", function () {
-            const newTheme = this.checked ? "dark" : "light"
-            document.documentElement.setAttribute("data-bs-theme", newTheme)
-            localStorage.setItem("theme", newTheme)
-        })
+            const newTheme = this.checked ? "dark" : "light";
+            document.documentElement.setAttribute("data-bs-theme", newTheme);
+            localStorage.setItem("theme", newTheme);
+        });
     }
-})
+});
